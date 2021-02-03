63b2270e6e189aa66ff835da42a7c570bd8e9a7e
ariellalowenstein <77687124+ariellalowenstein@users.noreply.github.com>
2021-02-03 13:24:05 -0500

    setup

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,29 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Schedule',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Calendar',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Me',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
+
+export default MenuItems;
